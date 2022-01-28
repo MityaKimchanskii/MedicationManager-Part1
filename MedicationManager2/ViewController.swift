74f7f3174e41f77b983c9473e6f8ac06e3f6185f
Dmitrii Kim <mitya_kim@icloud.com>
2022-01-27 17:35:15 -0800

    Initial Commit

---
+++ MedicationManager2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MedicationManager2
+//
+//  Created by Mitya Kim on 1/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
